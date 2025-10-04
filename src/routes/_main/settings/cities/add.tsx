7a9c766fae05cfbaf6cc33cbd6be6c020ea6a2ee
Amr-Zain <amrzain121@gmail.com>
2025-10-04 13:55:03 +0300

    added breadcrumb to the pages

--- src/routes/_main/settings/cities/add.tsx
+++ src/routes/_main/settings/cities/add.tsx
@@ -1,10 +1,26 @@
+import MainPageWrapper, {
+  breadcrumbItem,
+} from '@/components/layout/MainPageWrapper'
 import CityForm from '@/components/pagesComponents/Settings/cities/CityFrom'
 import { createFileRoute } from '@tanstack/react-router'
+import { Building2, Home, Plus } from 'lucide-react'
+import { useTranslation } from 'react-i18next'
 
 export const Route = createFileRoute('/_main/settings/cities/add')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <CityForm />
+  const { t } = useTranslation()
+  const breadcrumbItems: breadcrumbItem[] = [
+    { label: t('pages.home'), icon: <Home />, to: '/' },
+    { label: t('pages.cities'), icon: <Building2 />, to: '/settings/cities' },
+    { label: t('buttons.add'), icon: <Plus /> },
+  ]
+
+  return (
+    <MainPageWrapper breadcrumbItems={breadcrumbItems}>
+      <CityForm />
+    </MainPageWrapper>
+  )
 }
